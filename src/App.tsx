cf6cf232151c7673ebb1982a2ef564fa58cfdf4e
Mehdi Maleki <mosioc79@gmail.com>
2025-11-23 22:03:40 +0330

    feat: define theme
    issue #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Authenticated, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-import { useNotificationProvider } from "@refinedev/antd";
+import { RefineThemes, useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { App as AntdApp } from "antd";
+import { App as AntdApp, ConfigProvider } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 
 import { dataProvider, liveProvider, authProvider } from "./providers";
@@ -16,34 +16,36 @@ import { LoginPage } from "./routes/login";
 function App() {
   return (
     <BrowserRouter>
-      <RefineKbarProvider>
-        <AntdApp>
-          <DevtoolsProvider>
-            <Refine
-              dataProvider={dataProvider}
-              liveProvider={liveProvider}
-              notificationProvider={useNotificationProvider}
-              routerProvider={routerProvider}
-              authProvider={authProvider}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                projectId: "86WOi9-huED18-lKC55a",
-                liveMode: "auto",
-              }}
-            >
-              <Routes>
-                <Route path="/login" element={<LoginPage />} />
-              </Routes>
+      <ConfigProvider theme={RefineThemes.Green}>
+        <RefineKbarProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerProvider}
+                authProvider={authProvider}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  projectId: "86WOi9-huED18-lKC55a",
+                  liveMode: "auto",
+                }}
+              >
+                <Routes>
+                  <Route path="/login" element={<LoginPage />} />
+                </Routes>
 
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-            <DevtoolsPanel />
-          </DevtoolsProvider>
-        </AntdApp>
-      </RefineKbarProvider>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </RefineKbarProvider>
+      </ConfigProvider>
     </BrowserRouter>
   );
 }
