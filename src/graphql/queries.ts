e7bb77b3cbfecda1237368806fb653ffe60a7d18
Mehdi Maleki <mosioc79@gmail.com>
2025-11-23 19:42:52 +0330

    add graphql config, mutations and queries issue #4

---
+++ src/graphql/queries.ts
@@ -0,0 +1,36 @@
+import gql from "graphql-tag";
+
+// user query
+export const USERS_SELECT_QUERY = gql`
+  query UsersSelect(
+    $filter: UserFilter!
+    $sorting: [UserSort!]
+    $paging: OffsetPaging!
+  ) {
+    users(filter: $filter, sorting: $sorting, paging: $paging) {
+      totalCount
+      nodes {
+        id
+        name
+        avatarUrl
+      }
+    }
+  }
+`;
+
+// task stage query
+export const TASK_STAGES_SELECT_QUERY = gql`
+  query TaskStagesSelect(
+    $filter: TaskStageFilter!
+    $sorting: [TaskStageSort!]
+    $paging: OffsetPaging!
+  ) {
+    taskStages(filter: $filter, sorting: $sorting, paging: $paging) {
+      totalCount
+      nodes {
+        id
+        title
+      }
+    }
+  }
+`;
