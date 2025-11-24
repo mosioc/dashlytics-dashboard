ef48e8b39c74fbb18a7865b2bf996ca985ac0867
Mehdi Maleki <mosioc79@gmail.com>
2025-11-24 16:49:41 +0330

    add deals-chart queries and generate types
    issue #13

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -94,3 +94,37 @@ export type TaskStagesSelectQuery = {
     nodes: Array<Pick<Types.TaskStage, "id" | "title">>;
   };
 };
+
+export type DashboardDealsChartQueryVariables = Types.Exact<{
+  filter: Types.DealStageFilter;
+  sorting?: Types.InputMaybe<Array<Types.DealStageSort> | Types.DealStageSort>;
+  paging?: Types.InputMaybe<Types.OffsetPaging>;
+}>;
+
+export type DashboardDealsChartQuery = {
+  dealStages: Pick<Types.DealStageConnection, "totalCount"> & {
+    nodes: Array<
+      Pick<Types.DealStage, "id" | "title"> & {
+        dealsAggregate: Array<{
+          groupBy?: Types.Maybe<
+            Pick<
+              Types.DealStageDealsAggregateGroupBy,
+              "closeDateMonth" | "closeDateYear"
+            >
+          >;
+          sum?: Types.Maybe<Pick<Types.DealStageDealsSumAggregate, "value">>;
+        }>;
+      }
+    >;
+  };
+};
+
+export type DashboardTotalCountsQueryVariables = Types.Exact<{
+  [key: string]: never;
+}>;
+
+export type DashboardTotalCountsQuery = {
+  companies: Pick<Types.CompanyConnection, "totalCount">;
+  contacts: Pick<Types.ContactConnection, "totalCount">;
+  deals: Pick<Types.DealConnection, "totalCount">;
+};
