136c907055ecd63653b70013cd76cd92046e5a3a
Mehdi Maleki <mosioc79@gmail.com>
2025-11-24 20:36:40 +0330

    develop table for list
    issue #7

--- src/routes/companies/list/index.tsx
+++ src/routes/companies/list/index.tsx
@@ -2,13 +2,24 @@
  * displays companies in table with avatar, name, total deals amount, and action buttons
  * children prop renders nested routes like create/edit modals
  */
+
 import React from "react";
-import { List, useTable, CreateButton } from "@refinedev/antd";
-import { type HttpError, useGo } from "@refinedev/core";
+import {
+  CreateButton,
+  DeleteButton,
+  EditButton,
+  FilterDropdown,
+  List,
+  useTable,
+} from "@refinedev/antd";
+import { getDefaultFilter, type HttpError, useGo } from "@refinedev/core";
 import type { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { SearchOutlined } from "@ant-design/icons";
+import { CustomAvatar, PaginationTotal, Text } from "../../../components";
 import type { CompaniesListQuery } from "@/graphql/types";
+import { currencyNumber } from "../../../utilities";
 import { COMPANIES_LIST_QUERY } from "./queries";
-import { Table } from "antd";
+import { Input, Space, Table } from "antd";
 
 type Company = GetFieldsFromList<CompaniesListQuery>;
 
@@ -72,7 +83,64 @@ export const CompanyListPage = ({ children }: React.PropsWithChildren) => {
           />
         )}
       >
-        <Table></Table>
+        <Table
+          {...tableProps}
+          pagination={{
+            ...tableProps.pagination,
+            pageSizeOptions: ["12", "24", "48", "96"],
+            showTotal: (total) => (
+              <PaginationTotal total={total} entityName="companies" />
+            ),
+          }}
+          rowKey="id"
+        >
+          {/* company name column with avatar and search filter */}
+          <Table.Column<Company>
+            dataIndex="name"
+            title="Company title"
+            defaultFilteredValue={getDefaultFilter("id", filters)}
+            filterIcon={<SearchOutlined />}
+            filterDropdown={(props) => (
+              <FilterDropdown {...props}>
+                <Input placeholder="Search Company" />
+              </FilterDropdown>
+            )}
+            render={(_, record) => (
+              <Space>
+                <CustomAvatar
+                  shape="square"
+                  name={record.name}
+                  src={record.avatarUrl}
+                />
+                <Text style={{ whiteSpace: "nowrap" }}>{record.name}</Text>
+              </Space>
+            )}
+          />
+
+          {/* total revenue from open deals */}
+          <Table.Column<Company>
+            dataIndex="totalRevenue"
+            title="Open deals amount"
+            render={(_, company) => (
+              <Text>
+                {currencyNumber(company?.dealsAggregate?.[0]?.sum?.value || 0)}
+              </Text>
+            )}
+          />
+
+          {/* action buttons column */}
+          <Table.Column<Company>
+            fixed="right"
+            dataIndex="id"
+            title="Actions"
+            render={(value) => (
+              <Space>
+                <EditButton hideText size="small" recordItemId={value} />
+                <DeleteButton hideText size="small" recordItemId={value} />
+              </Space>
+            )}
+          />
+        </Table>
       </List>
 
       {/* render nested modal routes (create/edit) */}
