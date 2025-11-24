f4de4b9d001fc7a0e642f63601f6f11691c00d61
Mehdi Maleki <mosioc79@gmail.com>
2025-11-25 00:11:15 +0330

    refactor(deals-chart):component path addresses

--- src/routes/dashboard/components/deals-chart/index.tsx
+++ src/routes/dashboard/components/deals-chart/index.tsx
@@ -10,7 +10,7 @@ import type { GetFieldsFromList } from "@refinedev/nestjs-query";
 import { DollarOutlined } from "@ant-design/icons";
 import { Area, type AreaConfig } from "@ant-design/plots";
 import { Card } from "antd";
-import { Text } from "@/components";
+import { Text } from "../../../../components";
 import type { DashboardDealsChartQuery } from "@/graphql/types";
 import { DASHBOARD_DEALS_CHART_QUERY } from "./queries";
 import { mapDealsData } from "./utilities";
