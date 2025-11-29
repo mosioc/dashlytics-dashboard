6ca2af36b4e1260542aeb7734a988c2d4dcbda1f
Mehdi Maleki <mosioc79@gmail.com>
2025-11-29 21:25:35 +0330

    fix(test-wrapper): create new query client for each test
    issue #21

--- src/test/test-wrapper.tsx
+++ src/test/test-wrapper.tsx
@@ -3,22 +3,21 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider } from "antd";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      gcTime: 0,
-    },
-  },
-});
-
 export const TestWrapper: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        gcTime: 0,
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <ConfigProvider>{children}</ConfigProvider>
     </QueryClientProvider>
   );
 };
-
